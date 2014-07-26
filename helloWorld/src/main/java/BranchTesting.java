e7f2180bb0e3532261450fffda772907c097a675
nijiagit <nijiajndx@126.com>
2014-07-26 10:44:16 +0800

    ÎҼҴóÃų£´ò¿ª

--- helloWorld/src/main/java/BranchTesting.java
+++ helloWorld/src/main/java/BranchTesting.java
@@ -2,7 +2,8 @@
  * Created by nijia on 2014/7/26.
  */
 public class BranchTesting {
-    public static void print(){
+    public static void print() {
         System.out.println("branch testing");
+        System.out.println("hot");
     }
 }
